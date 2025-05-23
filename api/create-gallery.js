7caf9fdb6feedfcd30d499633f6b9f608500218d
qaisertariq <57266501+qaisertariq@users.noreply.github.com>
2025-05-23 15:34:03 +0500

    Update create-gallery.js

--- api/create-gallery.js
+++ api/create-gallery.js
@@ -31,7 +31,7 @@ module.exports = async (req, res) => {
           return crypto.createHmac('sha1', key).update(base_string).digest('base64');
         },
       });
-      const water = '484921';
+      const water = 484921;
       const url = `https://api.smugmug.com/api/v2/folder/user/${nickname}/${mainfolder}/${subfolder}!albums`;
       const method = 'POST';
       const data = {
