b66f793d36260461e6c6c3852613aaa0e766e25e
qaisertariq <57266501+qaisertariq@users.noreply.github.com>
2025-09-30 12:54:02 +0500

    Update update-gallery.js

--- api/update-gallery.js
+++ api/update-gallery.js
@@ -39,25 +39,7 @@ module.exports = async (req, res) => {
           Privacy: "Unlisted",
           WorldSearchable: "No",
           SmugSearchable: "No",
-          HideOwner: false,
-          LargestSize: "Original",  // Max display size
-          CanRank: false,  // Right-click message (inferred as OFF)
-          Watermarking: false,  // Apply watermark OFF
-          WatermarkUri: "",  // Clear selected watermark
-          AllowDownloads: true,  // Download buttons ON
-          OriginalSizes: true,  // Download size Original
-          Comments: false,  // Allow comments OFF
-          Share: true,  // Show sharing options ON
-          GuestUploading: true,  // Visitor shopping cart ON (if this maps; verify)
-          ProofDays: 0,  // Proof delay OFF
-          BoutiquePackaging: "Site-Setting",
-          PersonalDelivery: "Site-Setting",
-          BrandPackaging: true,  // ON
-          PrintmarkUri: "",  // Printmark -
-          Backprinting: "www.islanddreamproductions.com",
-          SortMethod: "Position",
-          SortDirection: "Descending",
-        
+                 
       };
       
 
